87ab89971e83c9419c5e69a5ad5dfff874448940
Timothy McPhillips <tmcphillips@absoluteflow.com>
2023-03-13 21:25:00 -0700

    Refactor 3rd demo

--- demo/03-gpg-encrypt-file/run.sh
+++ demo/03-gpg-encrypt-file/run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+
+PRIVATE_KEY_FILE=data/private.asc
+CLEAR_MESSAGE_FILE=data/message.txt
+ENCRYPTED_MESSAGE_FILE=products/message.asc
+
+
 # ------------------------------------------------------------------------------
 
-bash_cell 'delete gnupg home directory' << 'END_CELL'
+bash_cell 'delete gnupg home directory' << END_CELL
 
 # delete contents of the .gnupg directory for this REPRO
 gnupg-runtime.purge-keys
@@ -11,35 +17,35 @@ END_CELL
 
 # ------------------------------------------------------------------------------
 
-bash_cell 'import a private key' << 'END_CELL'
+bash_cell 'import a private key and bundled public key' << END_CELL
 
-gpg --import --pinentry-mode loopback --passphrase=repro data/private.asc
+# import the private key file
+gpg --import --pinentry-mode loopback --passphrase=repro ${PRIVATE_KEY_FILE}
 
 # list the gpg keys
 gpg --list-keys
 
-# show the public key that was bundled with the private key
+# show the public key that was bundled in the private key file
 gpg --export --armor repro@repros.dev
 
 END_CELL
 
 # ------------------------------------------------------------------------------
 
-bash_cell 'encrypt the message in a text file' << 'END_CELL'
+bash_cell 'encrypt the message in a text file using the public key for repro@repros.dev' << END_CELL
 
-ENCRYPTED_FILE=products/message.asc
-rm -f ${ENCRYPTED_FILE}
+rm -f ${ENCRYPTED_MESSAGE_FILE}
 
-gpg --encrypt --always-trust --armor --recipient repro@repros.dev --output ${ENCRYPTED_FILE} data/message.txt
+gpg --encrypt --always-trust --armor --recipient repro@repros.dev --output ${ENCRYPTED_MESSAGE_FILE} ${CLEAR_MESSAGE_FILE}
 
-cat ${ENCRYPTED_FILE}
+cat ${ENCRYPTED_MESSAGE_FILE}
 
 END_CELL
 
 # ------------------------------------------------------------------------------
 
-bash_cell 'decrypt the message' << 'END_CELL'
+bash_cell 'decrypt the message using the private key for repro@repros.dev' << END_CELL
 
-gpg --decrypt --pinentry-mode loopback --passphrase=repro products/message.asc
+gpg --decrypt --pinentry-mode loopback --passphrase=repro ${ENCRYPTED_MESSAGE_FILE}
 
 END_CELL
